49fb52bf00b62b86773a49442017ecb595724651
leon-sabir <leonsabirwm@gmail.com>
2022-05-01 04:33:12 +0600

    API created for single item

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
     });
+    app.get('/myitem/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : ObjectId(id)}
+        console.log(id);
+        const result = await itemsCollection.findOne(query);
+        res.send(result);
+    });
 
     app.post('/items',async(req,res)=>{
         const data = req.body;
