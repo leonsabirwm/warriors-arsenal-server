30ea2c19a905ca14029cb5954157c00bd3564a5c
leon-sabir <leonsabirwm@gmail.com>
2022-05-04 16:28:41 +0600

    some changes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function run() {
     app.get('/inventoryitem/:id',async(req,res)=>{
         const id = req.params.id;
         const query = {_id : ObjectId(id)}
-        console.log(id);
+        
         const result = await itemsCollection.findOne(query);
         res.send(result);
     });
@@ -50,7 +50,7 @@ async function run() {
     app.put('/inventoryitem/:id',async(req,res)=>{
         const id = req.params.id;
         const quantity = req.body.newQuantity;
-        console.log(quantity);
+        
         const filter = {_id : ObjectId(id)}
         const option = {upsert:true};
         const update = {
